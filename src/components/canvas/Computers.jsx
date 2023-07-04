f70802c34e69dd5059e897b6a46fb43e6489eef3
Pandey Sushmit Kumar <93032213+pandey-sushmit@users.noreply.github.com>
2023-07-05 01:53:03 +0530

    Add files via upload

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -25,8 +25,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={1} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
+        scale={isMobile ? 0.3 : 0.75}
+        position={isMobile ? [0, -1.5, -0.5] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -35,7 +35,7 @@ const Computers = ({ isMobile }) => {
 
 const ComputersCanvas = () => {
   const [isMobile, setIsMobile] = useState(false);
-
+  console.log(isMobile);
   useEffect(() => {
     // Add a listener for changes to the screen size
     const mediaQuery = window.matchMedia("(max-width: 500px)");
